a6d06a3a897f51dab6a973fc3ebbfe049956e99e
Syed Himaayath <syedhimaayath155@gmail.com>
2018-08-30 18:01:40 +0530

    footer

--- js/main.js
+++ js/main.js
@@ -33,6 +33,13 @@
 
 
 $(document).on("scroll", function(){
+
+	// var wScroll = $(this).scrollTop()
+	// var c1 = document.querySelector('.c01')
+	// c1.style.top=wScroll*0.1+'px';
+	// var c2 = document.querySelector('.c02')
+	// c2.style.top=wScroll*0.15+'px';
+	
 	if($(document).scrollTop() > 100){
 	  $(".logo_img").addClass("shrink");
 	  $(".navbar-nav").addClass("desc")
