930221761093a732aa1d88c864b2e2935a28c4c7
Syed Himaayath <syedhimaayath155@gmail.com>
2018-08-30 14:16:07 +0530

    header shrink func

--- js/main.js
+++ js/main.js
@@ -3,15 +3,58 @@
 	
 // }
 
-$(window).on('scroll',function(){
 
-	var wScroll = $(this).scrollTop()
-	var c1 = document.querySelector('.c01')
-	c1.style.top=wScroll*0.1+'px';
-	var c2 = document.querySelector('.c02')
-	c2.style.top=wScroll*0.15+'px';
-	
+// $(function(){
+//     $('.min-head').data('size','big');
+// });
+
+// $(window).scroll(function(){
+//     if($(document).scrollTop() > 0)
+//     {
+//         if($('.min-head').data('size') == 'big')
+//         {
+//             $('.min-head').data('size','small');
+//             $('.min-head').stop().animate({
+//                 height:'40px'
+//             },600);
+//         }
+//     }
+//     else
+//     {
+//         if($('.min-head').data('size') == 'small')
+//         {
+//             $('.min-head').data('size','big');
+//             $('.min-head').stop().animate({
+//                 height:'100px'
+//             },600);
+//         }  
+//     }
+// });
+
+
+$(document).on("scroll", function(){
+	if($(document).scrollTop() > 100){
+	  $(".logo_img").addClass("shrink");
+	  $(".navbar-nav").addClass("desc")
+	  $(".myheader").addClass("rem")
+	}
+	else
+	{
+		$(".logo_img").removeClass("shrink");
+		$(".navbar-nav").removeClass("desc")
+		$(".myheader").removeClass("rem")
+	}
 });
+
+// $(window).on('scroll',function(){
+
+// 	var wScroll = $(this).scrollTop()
+// 	var c1 = document.querySelector('.c01')
+// 	c1.style.top=wScroll*0.1+'px';
+// 	var c2 = document.querySelector('.c02')
+// 	c2.style.top=wScroll*0.15+'px';
+	
+// });
 
 $('.closeBt').on('click',function(){
 	$('.collapse').removeClass('in')
